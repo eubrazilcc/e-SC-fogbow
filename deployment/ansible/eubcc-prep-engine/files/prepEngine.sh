00be67eabb2c92c3e6222baf313666899a61be4a
Jacek <jacek.cala@gmail.com>
2015-12-10 14:19:10 +0000

    Updated the prepEngine script to accommodate to the latest changes in the system.

--- deployment/ansible/eubcc-prep-engine/files/prepEngine.sh
+++ deployment/ansible/eubcc-prep-engine/files/prepEngine.sh
@@ -1,35 +1,105 @@
 #!/bin/bash
 
-PORT_TO_CHECK=5445
-TIMEOUT=5
+# Global variables
 ENGINE_XML=/usr/local/eSC-engine/etc/engine.xml
+TIMEOUT=3
 
-# An operation to set the server address in the engine.xml
-set_server_addr()
+# An operation to set the server address and jms port in engine.xml
+config_engine()
 {
-    SRV_ADDR=$1
+    SRV_URL=$1
+    JMS_PORT=$2
+    
+    # The assumption is that the URL is correct
+    SRV_ADDR=$(echo $SRV_URL | cut -d '/' -f 3 - | cut -d '@' -f 2 - | cut -d ':' -f 1)
+    echo "Extracted server address is: $SRV_ADDR"
+    
+    if [ -z $SRV_ADDR ] ; then
+        echo "Invalid server address"
+        exit 3
+    fi
 
-    xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Engine']/Parameter[@Name='APIHost']/@Value" -v $SRV_ADDR $ENGINE_XML
+    xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Engine']/Parameter[@Name='ServerBaseURL']/@Value" -v $SRV_URL $ENGINE_XML
+
     xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Performance']/Parameter[@Name='JMSServerHost']/@Value" -v $SRV_ADDR $ENGINE_XML
     xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='JMS']/Parameter[@Name='JMSServer']/@Value" -v $SRV_ADDR $ENGINE_XML
     xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Provenance']/Parameter[@Name='JMSServerHost']/@Value" -v $SRV_ADDR $ENGINE_XML
+
+    xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Performance']/Parameter[@Name='JMSServerPort']/@Value" -v $JMS_PORT $ENGINE_XML
+    xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='JMS']/Parameter[@Name='JMSPort']/@Value" -v $JMS_PORT $ENGINE_XML
+    xmlstarlet ed -L -u "/PipelineDocument/Data/Parameter[@Name='Properties']/Parameter[@Name='SystemProperties']/Parameter[@Name='Provenance']/Parameter[@Name='JMSServerPort']/@Value" -v $JMS_PORT $ENGINE_XML
 }
 
-if [ $# -lt 1 ] ; then
-    echo "Missing argument. At least one IP address of the e-SC server is needed"
+# An operation to check the current availability of the server
+check_server()
+{
+    SRV_URL=$1
+    JMS_PORT=$2
+    
+    # The assumption is that the URL is correct
+    SRV_ADDR_PORT=$(echo $SRV_URL | cut -d '/' -f 3 - | cut -d '@' -f 2 -)
+    SRV_ADDR=$(echo $SRV_ADDR_PORT | cut -d ':' -f 1 -s)
+    if [ -z $SRV_ADDR ] ; then
+        SRV_ADDR=$(echo $SRV_ADDR_PORT | cut -d ':' -f 1)
+        PROTO=$(echo $SRV_URL | cut -d ':' -f 1 -s)
+        if [ "$PROTO" == "http" ] ; then
+            SRV_PORT=80
+        elif [ "$PROTO" == "https" ] ; then
+            SRV_PORT=443
+        else
+            echo "Unsupported protocol $PROTO in URL '$SRV_URL'"
+            exit 2
+        fi
+    else
+        SRV_PORT=$(echo $SRV_ADDR_PORT | cut -d ':' -f 2 -s)
+    fi
+
+    echo "Checking availability at address: '$SRV_ADDR' for ports: $SRV_PORT, $JMS_PORT"
+    if [ -z $SRV_ADDR ] ; then
+        echo "Invalid server address"
+        #exit 3
+    fi
+
+    check_port $SRV_ADDR $SRV_PORT 3
+    check_port $SRV_ADDR $JMS_PORT 3
+}
+
+check_port()
+{
+    HOST=$1
+    PORT=$2
+    TIMEOUT=$3
+    
+    if [ -z $TIMEOUT ] ; then
+        TIMEOUT=5
+    fi
+
+    echo "Checking availability of ${HOST}:${PORT}, timeout = ${TIMEOUT}..."
+
+    nc -z -w $TIMEOUT $HOST $PORT
+    if [ $? -eq 0 ] ; then
+        echo "[OK] ${HOST}:${PORT} is open."
+        return 0
+    else
+        echo "[FAILED] ${HOST}:${PORT} is unreachable at the moment."
+        return 1
+    fi
+}
+
+if [ $# -lt 2 ] ; then
+    echo "Missing some arguments: $0 <serverBaseURL> <jmsPort>"
     exit 1
 fi
 
-for ARG in "$@" ; do
-    echo "Checking whether host $ARG opened port $PORT_TO_CHECK (assuming it's JMS)..."
-    nc -z -w $TIMEOUT $ARG $PORT_TO_CHECK
-    if [ $? -eq 0 ] ; then
-        echo "Ok. Port $PORT_TO_CHECK is open. Using $ARG as the e-SC server address."
-        service eSC-engine stop
-        set_server_addr $ARG
-        update-rc.d eSC-engine enable
-        service eSC-engine start
-    else
-        echo "Failed. $ARG:$PORT_TO_CHECK is unreachable."
-    fi
-done
+SERVER_BASE_URL=$1
+JMS_PORT=$2
+
+check_server $SERVER_BASE_URL $JMS_PORT
+
+echo "Using server URL: $SERVER_BASE_URL and JMS port: $JMS_PORT to configure e-SC engine at: $ENGINE_XML"
+service eSC-engine stop
+
+config_engine $SERVER_BASE_URL $JMS_PORT
+
+update-rc.d eSC-engine enable
+service eSC-engine start
